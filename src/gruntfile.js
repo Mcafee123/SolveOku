138504f5f5cacfcb34ee21ae04badcc1964b4a69
Martin Affolter <martin@affolter.net>
2015-02-07 08:22:57 +0100

    setup:
    - bower
    - version reading

--- src/gruntfile.js
+++ src/gruntfile.js
@@ -4,4 +4,10 @@ module.exports = function (grunt) {
     var version;
     
     version = grunt.file.read('package.json');
+    version = JSON.parse(version);
+    version = version.version;
+    grunt.file.write('solveoku/app.version.js', "/*globals angular*/\nangular.module('solveoku').factory('versionSvc', function () {\n    'use strict';\n    return {\n        SolveOkuVersion: '" + version.trim() + "'\n    };\n});");
+    
+    grunt.registerTask('default', function () {});
+    
 };
---
+++ src/solveoku/app.version.js
@@ -0,0 +1,7 @@
+/*globals angular*/
+angular.module('solveoku').factory('versionSvc', function () {
+    'use strict';
+    return {
+        AVGBuildVersion: '0.0.1'
+    };
+});
